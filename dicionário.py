6de9bee646a33e19c7566ce4cc78d898a276b6f2
Calara <m.clarafreitasalmeida14@gmail.com>
2025-08-25 11:48:34 -0300

    atividade Python
    aulas de dicionário

---
+++ dicionário.py
@@ -0,0 +1,13 @@
+tabela = {"Alface": 0.45,
+          "Batata": 1.20,
+          "Tomate": 2.30,
+          "Feijão": 1.50 }
+print(tabela["Tomate"])
+print(tabela["Batata"])
+print(tabela["Alface"])
+print(tabela["Feijão"])
+tabela["Cebola"] = 1.20
+print(tabela)
+print("Manga" in tabela)
+print("Morango" in tabela)
+print("Feijão" in tabela)
