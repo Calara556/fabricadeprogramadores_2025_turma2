0b388960b0372fcf7946c73e0d0119f2b57a0032
Calara <m.clarafreitasalmeida14@gmail.com>
2025-09-01 10:53:41 -0300

    atividade python
    lição de automação

---
+++ automação.py
@@ -0,0 +1,52 @@
+import pyautogui, mouseinfo, time
+
+'''mouseinfo.mouseInfo()'''
+
+pyautogui.moveTo(1202,1065, duration=1)
+pyautogui.click()
+pyautogui.sleep(4)
+pyautogui.moveTo(990,813, duration=1)
+pyautogui.click()
+pyautogui.sleep(4)
+pyautogui.moveTo(975,339, duration=1)
+pyautogui.click()
+pyautogui.sleep(4)
+pyautogui.moveTo(1073,470, duration=1)
+pyautogui.click()
+pyautogui.sleep(4)
+pyautogui.moveTo(1068,498, duration=1)
+pyautogui.click()
+pyautogui.sleep(4)
+pyautogui.moveTo(1074,504, duration=1)
+pyautogui.click()
+pyautogui.sleep(4)
+pyautogui.moveTo(1075,507, duration=1)
+pyautogui.click()
+pyautogui.sleep(4)
+pyautogui.moveTo(1084,312, duration=1)
+pyautogui.click()
+pyautogui.sleep(4)
+pyautogui.moveTo(1076,256, duration=1)
+pyautogui.click()
+pyautogui.sleep(4)
+pyautogui.moveTo(1283,767, duration=1)
+pyautogui.click()
+pyautogui.sleep(4)
+pyautogui.write("Hello guys")
+pyautogui.sleep(4)
+pyautogui.moveTo(1887,102, duration=1)
+pyautogui.click()
+pyautogui.sleep(4)
+
+
+
+
+
+
+
+
+
+
+
+
+
